65f55248992d18e2e41f242d2f096d119c1a60b2
jaredthomasbell <jaredthomasbell@proton.me>
2023-04-16 16:36:49 -0500

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
-use tokio::*;
+
 use url::Url;
 use thirtyfour::prelude::*;
 use tokio::time::sleep;
-use std::{error::Error, result};
+use reqwest::blocking::Client;
+use select::document::Document;
+use select::predicate::{Attr};
+use std::collections::HashSet;
+use std::iter::FromIterator;
 use tokio::time::timeout;
+use select::predicate::{Name};
 use std::time::Duration;
 use thirtyfour::common::capabilities::chrome::ChromeCapabilities;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut chrome_capabilities = ChromeCapabilities::new();
-    chrome_capabilities.set_headless();
-    let driver = WebDriver::new("http://localhost:12345", chrome_capabilities).await?;
     
    //get init URLS 
    let urls: Vec<&str> = vec!["http://stpaulsburkittsville.org/", "http://pvbc1.com/"];
-   let sub = subpages(driver, urls, 3).await?;
-   Ok(println!("{:?}", sub))
+   let mut result = Vec::new();
 
+   for url in  urls {
+        match find_subpages(url).await{
+            Ok(subpages) => {
+                result.push(subpages);
+
+            }
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+    Ok(println!("{:?}", result))
 }
 
 
 
-async fn subpages(driver: WebDriver, urls: Vec<&str>, depth: usize) ->  Result<Vec<Vec<(String, String)>>, Box<dyn std::error::Error>> {
+async fn subpages(driver: WebDriver, urls: Vec<&str>, _depth: usize) ->  Result<Vec<Vec<(String, String)>>, Box<dyn std::error::Error>> {
     let mut result: Vec<Vec<(String, String)>> = Vec::new();
     for url in urls {
         let mut links: Vec<(String, String)> = Vec::new();
@@ -31,7 +42,7 @@ async fn subpages(driver: WebDriver, urls: Vec<&str>, depth: usize) ->  Result<V
             Ok(_) => {
                 sleep(Duration::from_secs(4)).await;
                 let elem = driver.find_element(By::Tag("a")).await?;
-                let base_url = Url::parse(url)?;
+                let _base_url = Url::parse(url)?;
                 if let Some(href) = elem.attr("href").await? { links.push((url.to_string(), href)); }
                     
                 } 
@@ -39,5 +50,44 @@ async fn subpages(driver: WebDriver, urls: Vec<&str>, depth: usize) ->  Result<V
         }
         result.push(links);
     } 
-    Ok((result))
+    Ok(result)
+}
+
+async fn find_subpages(url: &str) -> Result<HashSet<String>, Box<dyn std::error::Error>> {
+    let root_url = Url::parse(url)?;
+    let mut visited = HashSet::new();
+    let mut to_visit = HashSet::from_iter(vec![url.to_string()]);
+
+    for _ in 0..2 {
+        let mut new_pages = HashSet::new();
+        for url in to_visit {
+            if visited.contains(&url) {
+                continue;
+            }
+            visited.insert(url.clone());
+
+            let response = Client::new().get(&url).send()?;
+            tokio::time::sleep(Duration::from_secs(2)).await;
+            let document = Document::from_read(response.text()?.as_bytes())?;
+
+            for link in document.find(Attr("href", ())) {
+                if let Some(link_url) = link.attr("href") {
+                    let base_url = Url::parse(&url)?;
+                    if let Ok(abs_url) = base_url.join(link_url) {
+                        // Check if the origin of the absolute URL is the same as the root URL
+                        if abs_url.origin() == root_url.origin() {
+                            let abs_url_string = abs_url.into_string();
+                            // Check if the URL does not end with ".jpg"
+                            if !abs_url_string.ends_with(".jpg") {
+                                new_pages.insert(abs_url_string);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        to_visit = new_pages;
+    }
+
+    Ok(visited)
 }
